ada2769973399cac40e430e73c78d23a18d74d5e
Jeremy G Diamond <jeremygdiamond@gmail.com>
2025-06-04 15:34:00 +0200

    exp shell

--- raplpermission.sh
+++ raplpermission.sh
@@ -4,3 +4,6 @@ sudo chmod -R a+r /sys/class/powercap/intel-rapl
 
 sudo chgrp users /sys/class/powercap/intel-rapl/subsystem/intel-rapl-mmio:*/energy_uj
 sudo chmod g+r /sys/class/powercap/intel-rapl/subsystem/intel-rapl-mmio:*/energy_uj
+
+sudo chgrp -R msr /dev/cpu/*/msr;
+sudo chmod g+r /dev/cpu/*/msr;
